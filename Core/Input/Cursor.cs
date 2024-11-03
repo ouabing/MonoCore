115bc18b2b3942f0e80bd959cd3402b04d565105
abing <ouabing@users.noreply.github.com>
2024-11-03 21:23:21 +0800

    feat: support cursor AABB detection

--- Core/Input/Cursor.cs
+++ Core/Input/Cursor.cs
@@ -1,8 +1,23 @@
+using System.Collections.Generic;
 using Microsoft.Xna.Framework;
+using nkast.Aether.Physics2D.Collision;
+using nkast.Aether.Physics2D.Dynamics;
 
 namespace G;
 
 public abstract class Cursor : Component
 {
   public override Vector2 PreviousPosition => Core.Input.PreviousCursorWorldPosition;
+
+  public static List<Component> QueryAABBs(Category category = Category.All, bool fixedCamera = true)
+  {
+    var position = fixedCamera ? Core.Input.CursorScreenPosition : Core.Input.CursorWorldPosition;
+    var upperBound = position.ToMeterVector2();
+    AABB aabb = new()
+    {
+      LowerBound = upperBound + new Vector2(1f, 1f).ToMeterVector2(),
+      UpperBound = upperBound
+    };
+    return Core.Physics.QueryAABBs(aabb, category);
+  }
 }
